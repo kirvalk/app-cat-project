73f9cf07de53e03ad6b651ad1e3671b0868181cf
Kirill Karagedov <karageodvk@gmail.com>
2018-04-07 14:01:07 +0700

    added task 2

--- app/js/script.js
+++ app/js/script.js
@@ -6,3 +6,17 @@ function setColor (elemSet, color) {
 }
 
 setColor(allElems, "green");
+
+
+
+function getRandomColor() {
+	const r = Math.floor(Math.random()*255);
+	const g = Math.floor(Math.random()*255);
+	const b = Math.floor(Math.random()*255);
+	const result = "rgb("+ r + "," + g + "," + b + ")";
+	return result;
+}
+
+const intervalId = setInterval(function(){
+	setColor(allElems, getRandomColor())
+}, 2000);
