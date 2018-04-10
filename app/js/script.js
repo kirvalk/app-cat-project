2aabd9f341933e6816ecb48246df59c6f8b7e0dc
Kirill Karagedov <karageodvk@gmail.com>
2018-04-10 09:33:52 +0700

    changed indentations

--- app/js/script.js
+++ app/js/script.js
@@ -1,45 +1,45 @@
 document.addEventListener('DOMContentLoaded', function(){
-	const allElems = document.querySelectorAll('*'),
-	      REMOVE_INTERVAL_ID = 600;
-	      SET_INTERVAL_ID = 500;
-	      
-	setInterval(function(){
-		setElemColor(getRandomElem(allElems), getRandomColor());
-	}, SET_INTERVAL_ID);
+  const allElems = document.querySelectorAll('*'),
+  REMOVE_INTERVAL_ID = 600;
+  SET_INTERVAL_ID = 500;
+  
+  setInterval(function(){
+    setElemColor(getRandomElem(allElems), getRandomColor());
+  }, SET_INTERVAL_ID);
 
-	setInterval(function(){
-		removeColor(getRandomChangedElem());
-	}, REMOVE_INTERVAL_ID);
+  setInterval(function(){
+    removeColor(getRandomChangedElem());
+  }, REMOVE_INTERVAL_ID);
 
-	function getRandomElem (elemSet) {
-		const rnd = Math.floor(Math.random()*elemSet.length);
-		return elemSet[rnd];
-	}
+  function getRandomElem (elemSet) {
+    const rnd = Math.floor(Math.random()*elemSet.length);
+    return elemSet[rnd];
+  }
 
-	function setElemColor (elem, color) {
-		elem.style.backgroundColor = color;
-		elem.classList.add('changed');
-	}
+  function setElemColor (elem, color) {
+    elem.style.backgroundColor = color;
+    elem.classList.add('changed');
+  }
 
-	function getRandomChangedElem () {
-		const changedElems = document.querySelectorAll('.changed');
-		return getRandomElem(changedElems);	
-	}
+  function getRandomChangedElem () {
+    const changedElems = document.querySelectorAll('.changed');
+    return getRandomElem(changedElems); 
+  }
 
-	function removeColor (elem) {
-		elem.style.backgroundColor = '';
-		elem.classList.remove('changed');
-	}
+  function removeColor (elem) {
+    elem.style.backgroundColor = '';
+    elem.classList.remove('changed');
+  }
 
-	function getRandomRgbNum () {
-		return Math.floor(Math.random()*256);
-	}
+  function getRandomRgbNum () {
+    return Math.floor(Math.random()*256);
+  }
 
-	function getRandomColor () {
-		const r = getRandomRgbNum(),
-		      g = getRandomRgbNum(),
-			  b = getRandomRgbNum();
-		const result = `rgb(${r}, ${g}, ${b})`;
-		return result;
-	}
+  function getRandomColor () {
+    const r = getRandomRgbNum(),
+    g = getRandomRgbNum(),
+    b = getRandomRgbNum();
+    const result = `rgb(${r}, ${g}, ${b})`;
+    return result;
+  }
 });
